fc1bf9b4976f5139575fd2f50995c422e9f9314a
Alina Hurchyna <127059230+AlinaHurchyna@users.noreply.github.com>
2023-12-26 23:02:51 +0100

    init

--- src/main/java/alina_hurchyna_ah/driver/MyController.java
+++ src/main/java/alina_hurchyna_ah/driver/MyController.java
@@ -1,15 +1,14 @@
 package alina_hurchyna_ah.driver;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+
 public class MyController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello from the API!";
+    @GetMapping
+    public String home() {
+        return "Welcome to the home page!";
     }
 }
